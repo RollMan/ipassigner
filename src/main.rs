4c19524a1c3a31f83fb2ea09e414ba8eb80086ff
Yohei Shimmyo <qwpmb554@gmail.com>
2018-03-30 17:49:38 +0900

    finish to implement request part but compile error.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 extern crate regex;
 extern crate nickel;
 extern crate postgres;
+extern crate rustc_serialize;
 
+use std::collections::BTreeMap;
 use nickel::Nickel;
-use nickel::{Request, Response, MiddlewareResult, HttpRouter};
+use nickel::{Request, Response, MiddlewareResult, HttpRouter, JsonBody, MediaType};
+use nickel::status::StatusCOde;
 use regex::Regex;
 use postgres::{Connection, SslMode};
+use rustc_serialize::json::{Json, ToJson};
 
 struct Address {
     address: String,
-    user_name: String,
-    user_history: Vec<String>,
+    user_id: i32,
+    user_history: Vec<i32>,
+}
+
+impl ToJson for Address {
+    fn to_json(&self) -> Json{
+        let mut map = BTreeMap::new();
+        map.insert("address".to_string(), self.address.to_json());
+        map.insert("user_name".to_string(), self.user_name.to_json());
+        map.insert("user_history".to_string(), self.user_history.to_json());
+        Json::Object(map)
+    }
 }
 
 struct User {
@@ -19,13 +33,58 @@ struct User {
     student_id: String,
 }
 
+impl ToJson for User {
+    fn to_json(&self) -> Json{
+        let mut map = BTreeMap::new();
+        map.insert("id".to_string(), self.id.to_json());
+        map.insert("name".to_string(), self.name.to_json());
+        map.insert("student_id".to_string(), self.student_id.to_json());
+        Json::Object(map)
+    }
+}
+
+struct StatusResult{
+    success: bool,
+    operation: String,
+    address: String,
+}
+
+impl ToJson for StatusResult {
+    fn to_json(&self) -> Json{
+        let mut map = BTreeMap::new();
+        map.insert("success".to_string, self.success.to_json());
+        map.insert("operation".to_string, self.operation.to_json());
+        map.insert("address".to_string, self.address.to_json());
+        JSON::Object(map)
+    }
+}
+
 fn status(addr_db: &Connection, user_db: &Connection) -> for<'r, 'mw, 'conn> fn (&mut Request<'mw, 'conn>, Response<'mw>) -> MiddlewareResult<'mw>{
 
     fn status_<'mw, 'conn>(request: &mut Request<'mw, 'conn>, res: Response<'mw>) -> MiddlewareResult<'mw>{
-        let username = request.param("username").unwrap();
+        let userid = request.param("username").unwrap();
         let operation = request.param("operation").unwrap();
+        let mut res_status: StatusResult;
 
-        res.send( format!("{} requested {}.", username, operation) )
+        if(operation == "request"){
+            let available_addresses = addr_db.query("SELECT * FROM addresses WHERE user_id IS NULL", &[]).unwrap();
+            if(available_addresses.is_epmry()){
+                return res.send("{success: false}");
+            }
+            let mut entry: postgres::Row = available_addresses[0];
+            let address = entry.get(0);
+            let user_id = entry.get(1);
+            addr_db.execute("UPDATE addresses SET column = $1 WHERE address = $2", &[&use_id, &address]);
+
+            res_status = StatusResult(true, operation, address);
+        }else if(operation == "list"){
+
+        }else if(operation == "return"){
+
+        }else{
+            return res.error(StatusCOde::BadRequest, "No such operation");
+        }
+        res_status.to_json();
     }
 
     let res = status_;
